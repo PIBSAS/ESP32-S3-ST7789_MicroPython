a9d2267c46013823a179ca81963481fad58d2a10
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-26 18:44:29 -0300

    Add files via upload

---
+++ examples/configs/esp32s3-N16R8/tft_buttons.py
@@ -0,0 +1,9 @@
+from machine import Pin
+
+class Buttons():
+    def __init__(self):
+        self.left   = Pin(15, Pin.IN, Pin.PULL_UP)
+        self.right  = Pin(16, Pin.IN, Pin.PULL_UP)
+        self.hyper  = Pin(17, Pin.IN, Pin.PULL_UP)
+        self.thrust = Pin(18, Pin.IN, Pin.PULL_UP)
+        self.fire   = Pin(21, Pin.IN, Pin.PULL_UP)
