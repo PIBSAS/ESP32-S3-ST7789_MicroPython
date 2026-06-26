330c9ea8b689a7991d80d1419b08d5583058f086
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-26 18:57:50 -0300

    Update tft_buttons.py

--- examples/configs/esp32s3-N16R8/tft_buttons.py
+++ examples/configs/esp32s3-N16R8/tft_buttons.py
@@ -2,6 +2,7 @@ from machine import Pin
 
 class Buttons():
     def __init__(self):
+        self.name = "esp32s3-n16r8"
         self.left   = Pin(15, Pin.IN, Pin.PULL_UP)
         self.right  = Pin(16, Pin.IN, Pin.PULL_UP)
         self.hyper  = Pin(17, Pin.IN, Pin.PULL_UP)
