2eae0a7a977686e20f8600614d60db9790202b26
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-28 20:24:33 -0300

    Update pinball.py

--- examples/pinball.py
+++ examples/pinball.py
@@ -1,44 +1,45 @@
 """
-pinball.py - Minimal pinball game in MicroPython based on code from Ten Minute Physics Tutorial
-"How to write a pinball simulation" for using the display driver from
-https://github.com/russhughes/st7789_mpy.
+pinball.py - 
 
-Tutorial Links:
-https://matthias-research.github.io/pages/tenMinutePhysics/
-https://youtu.be/NhVUCsXp-Uo
-
-Gameplay Video:
-https://youtu.be/y0B3i_UmEU8
-
-Requires:
-    tft_config.py for display configuration. See examples/configs
-    tft_buttons.py for button configuration. See examples/configs
-    OR modify the code for your own display and buttons.
-
-This file incorporates work covered by the following copyright and permission notice.
-Modifications and additions Copyright (c) 2022 Russ Hughes and released under the same
-terms as the original code.
-
-
-Copyright 2021 Matthias Müller - Ten Minute Physics
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    Required:
+    - fonts:
+        - vga.py
+        -vga_bold_16x32.py
+    - Button's module already in the firmwaare: tft_buttons.py
+    
+    Minimal pinball game in MicroPython based on code from Ten Minute Physics Tutorial
+    "How to write a pinball simulation" for using the display driver from https://github.com/russhughes/st7789_mpy.
+    
+    Tutorial Links:
+    - https://matthias-research.github.io/pages/tenMinutePhysics/
+    - https://youtu.be/NhVUCsXp-Uo
+    
+    Gameplay Video:
+    - https://youtu.be/y0B3i_UmEU8
+    
+    This file incorporates work covered by the following copyright and permission notice.
+    Modifications and additions Copyright (c) 2022 Russ Hughes and released under the same
+    terms as the original code.
+    
+    Copyright 2021 Matthias Müller - Ten Minute Physics
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+    of the Software, and to permit persons to whom the Software is furnished to do
+    so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 """
 
 # pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name
